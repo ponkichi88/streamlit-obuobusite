c176b17a6631cd05d3f95885d5b6ff1bf3ea2bef
ponkichi88 <24e1811j@shinshu-u.ac.jp>
2025-11-20 18:45:14 +0900

    Update 2_モザイクアート作成.py

--- pages/2_モザイクアート作成.py
+++ pages/2_モザイクアート作成.py
@@ -8,7 +8,7 @@ with st.sidebar:
     st.header("ページ移動")
     st.page_link("pages/1_アップロード.py", label="🖼️ タイル画像を投稿", icon=None) 
     st.page_link("pages/2_モザイクアート作成.py", label="🧩 モザイクアートを作成", icon=None) 
-    st.page_link("Home.py", label="🏠 ホームに戻る", icon=None)
+    st.page_link("ホーム.py", label="🏠 ホームに戻る", icon=None)
 
 st.set_page_config(
     page_title="モザイクアートを作成",
